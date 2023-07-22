4e5fcd8ec445ac0e6575161ee8579045e308ab22
rafiul41 <rafiul1994@gmail.com>
2023-07-22 13:15:30 +0600

    Testing route for API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/products', function () {
+    return 'Hello world';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
